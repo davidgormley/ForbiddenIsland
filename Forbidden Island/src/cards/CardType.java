c6229e58b1d6d0f98d7bdfb8969112caf4db5f41
Owen RH <61594228+arpeggiowen@users.noreply.github.com>
2020-11-24 19:17:06 +0000

    Cards implmentation v1
    - Cards have been implemented here with two class files, and two enums.
    
    -The base Card class contains common attributes and methods.
    
    -The IslandTile class extends then extends this.
    
    - Other card types  can then be implemented as instances of the base card class using with appropriate name and CardType Enum to differentiate.

---
+++ Forbidden Island/src/cards/CardType.java
@@ -0,0 +1,10 @@
+package cards;
+
+public enum CardType {
+	FLOOD,
+	HELI,				// Helicopter lift card
+	TILE,
+	TREASURE,
+	SANDBAG,
+	WATER_RISE
+}
--- Forbidden Island/src/cards/IslandTile.java
+++ Forbidden Island/src/cards/IslandTile.java
@@ -8,14 +8,51 @@
 
 package cards;
 
-public class IslandTile {
-	public String name;
-	// each tile has a flooded and unflooded side, should this be a boolean?
+public class IslandTile extends Card{
+	// DG: each tile has a flooded and unflooded side, should this be a boolean?
+	// ORH: It could also be in 'sunk' state. 'Flooded' Enum has been created. 
 	public Flooded flooded; 
+	public boolean hasTreasure;				// tracks whether tile contains a treasure
 	
 	// constructor
-	public IslandTile(String name) {
-		this.name = name;
+	public IslandTile(String name, CardType type, boolean hasTreasure) {
+		super(name,type);
 		this.flooded = Flooded.DRY;			// default state is dry
+		this.hasTreasure = hasTreasure;
 	}
+	
+	// method to change flood state
+	// ORH: will a check against flood state be made in player method before calling this method?
+	// i.e., is the last else / error statement here needed?
+	public void flood() {
+		if (flooded == Flooded.DRY) {
+			flooded = Flooded.FLOODED;}
+		else if (flooded == Flooded.FLOODED) {
+			flooded = Flooded.SUNK;}
+		else {
+			System.out.println("Tile is already sunk.");}	// invalid action
+	}
+	
+	// method to shore up a flooded tile
+	// ORH: will a check against flood state be made in player method before calling this method?
+	public void shoreUp() {
+		if (flooded == Flooded.FLOODED) {
+			flooded = Flooded.DRY;}
+		else {
+			System.out.println("Only flooded tiles may be shored up.");}	// invalid action
+	}
+	
+	// method to check whether a treasure can be captured
+	public boolean isLootable() {
+		if (hasTreasure == true) {
+			return true;}
+		else {
+			return false;}
+	}
+	
+	// method to invoke when capturing a treasure
+	public void loot() {
+		hasTreasure = false;
+	}
+	
 }
