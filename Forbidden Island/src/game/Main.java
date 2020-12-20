6c7274403acb6b0b7edac4c58db397049fd4652c
davidgormley <54341250+davidgormley@users.noreply.github.com>
2020-12-20 19:06:17 +0000

    Removed unnecessary Player Java files and started game set up logic in main

--- Forbidden Island/src/game/Main.java
+++ Forbidden Island/src/game/Main.java
@@ -8,14 +8,52 @@
 package game;
 import player.*;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int numberPlayers; // number of players between 2 - 4
+		int waterLevel; // water level between 1 - 5
+		
+		// Setting up the game
+		System.out.println("Setting up the game...\n");
+
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("How many players are playing? Enter integer between 2 and 4:");
+		numberPlayers = in.nextInt();
+	    System.out.println("There are " + numberPlayers + " players playing.\n");
+	    
+	    // Setting up the forbidden island board and assigning tiles to the board
+	    
+	    // ForbiddenIslandBoard = setUpBoard();
+	    
+	    // Setting up the island tiles that will start to sink
+	    
+	    // Create floodDeck and take top 6 cards, flip the corresponding 6 island tiles to flooded
+	    
+	    // Randomly assign a role to each player
+		System.out.println("Randomly assigning a role to each player.\n");
+		
+		// Begin handing out 2 treasure cards to each player
+		System.out.println("Handing out 2 Treasure Cards to each player.\n");
+		
+		// Set the water level
+		System.out.println("Set the water level between 1 - 5. Enter water level:");
+		waterLevel = in.nextInt();
+	    System.out.println("The water level is " + waterLevel);
+	    
+	    
+	    // Finish game set
+		System.out.println("Finished game set up. Get ready to play.\n");
+
 
 	}
 	
 	
-	Player samplePlayer = new Player("John");
+	// Player samplePlayer = new Player("John");
 
 }
