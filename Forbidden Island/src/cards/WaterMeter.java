020385aa2d571afbfe75e88ec8d7e821cd427c6f
Srinithi97 <srinithi.ramprasad@ucdconnect.ie>
2020-12-21 18:30:55 +0000

    commented

--- Forbidden Island/src/cards/WaterMeter.java
+++ Forbidden Island/src/cards/WaterMeter.java
@@ -1,12 +1,16 @@
 package cards;
 
 public class WaterMeter {
+	
+//	water rise card minumum level is 1
 private int level=1;
 
+//get the water rise card level
 public int getLevel() {
 	return level;
 }
 
+//set or increase the water rise level
 public void setLevel(int level) {
 	this.level = level;
 }
--- Forbidden Island/src/deck/Deck.java
+++ Forbidden Island/src/deck/Deck.java
@@ -10,29 +10,30 @@ import java.util.Collections;
 public class Deck<T> {
 	Stack<T> deck = new Stack<T>();
 	
-
+//add a card to the deck
 	public void addCard(Card e) {
 		this.deck.add((T) e);
 	}
 
 	
-
+//draw a card from the deck
 	public T drawCard() {
 
 		return deck.pop();
 	}
 
-
+//shuffle the deck 
 	public void shuffleDeck() {
 		Collections.shuffle(this.deck);
 	}
-
+//print the cards in the deck
 	public void printDeck() {
 		for (int i = 0; deck.size() > i; i++) {
 			System.out.println(this.deck.elementAt(i).toString());
 		}
 	}
 
+//	get the deck length
 	public int deckLength() {
 		return deck.size();
 	}
