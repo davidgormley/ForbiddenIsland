288c775af4ed6f1081bf60e99bf608c18a08d59f
Owen RH <61594228+arpeggiowen@users.noreply.github.com>
2020-12-21 00:45:13 +0000

    Game initial setup
    Added / expanded:
    - deck setup
    - initial flood card draw
    - player role and position assignment
    - initial treasure card draw

--- Forbidden Island/src/game/Main.java
+++ Forbidden Island/src/game/Main.java
@@ -55,31 +55,106 @@ public class Main {
 		
 		// Game loop
 		while (newGame == true) {
+			
+			/**
+			 * Initial game setup
+			 */
+			
+			// win flag
+			boolean win = false;
+			
+			// set number of players
 			System.out.println("How many players are playing? Enter integer between 2 and 4:");
 			numberPlayers = in.nextInt();
+			// TODO verify input
 		    System.out.println("There are " + numberPlayers + " players playing.\n");
 		    
-		    
+		    // set flood level
+			System.out.println("Set flood level? Enter integer between 1 and 5:");
+			numberPlayers = in.nextInt();
+			// TODO verify input
 		    
 		    // Setting up the forbidden island board and assigning tiles to the board
 		    Board gameboard = Board.getInstance();
 		    
-		    // Setting up the island tiles that will start to sink
+		    /**
+		   	 * Setting up the island tiles that will start to sink
+		     */
+		    
+		    // Create treasure deck and discard, then shuffle
+		    // Deck treasureDeck = new Deck();
+		    // Deck treasureDiscard = new Deck();
+		    // treasureDeck.shuffle();
 		    
 		    // Create floodDeck and take top 6 cards, flip the corresponding 6 island tiles to flooded
 		    // Deck floodDeck = new Deck();
+		    // Deck floodDiscard = new Deck();
 		    // floodDeck.fillWithTiles();
+		    // floodDeck.shuffle();
 		    
 		    for (int d = 0; d < 6; d ++) {
-		    	//int[2] T = gameboard.tileCoords(floodDeck.drawCard().getName());
-		    	//gameboard[T[0]][T[1]].flood();
+		    	// draw from flood deck
+		    	// IslandTile tmp = floodDeck.drawCard();
+		    	
+		    	// get position of that card on the board
+		    	// int[2] tmpPos = gameboard.tileCoords(tmp.getName());
+		    	
+		    	// flood it
+		    	// gameboard[tmpPos[0]][tmpPos[1]].flood();
+		    	
+		    	// add flood card to discard pile
+		    	// floodDiscard.addCard(tmp);
 		    }
 		    
 		    // Randomly assign a role to each player
 			System.out.println("Randomly assigning a role to each player.\n");
 			
+			/**
+				assign roles and starting positions
+			**/
+			for (int p = 0; p < numberPlayers; p++) {
+				// randomly assign a role
+				// player[p].assignRole();
+				
+				// set starting position on board based on role
+				// player[p].setStartPos(role);
+				
+				// tell the player which role they were assigned
+				// player[p].printRole();
+			}
+			
 			// Begin handing out 2 treasure cards to each player
 			System.out.println("Handing out 2 Treasure Cards to each player.\n");
+			
+			for (int p = 0; p < numberPlayers; p++) {
+				// draw treasure cards
+				// Card tmp1 = treasureDeck.drawCard();
+				/**
+				// check if water rise card is drawn and repeat draw as necessary
+				while (tmp1.getType() == CardType.Water_Rise){
+					treasureDeck.addCard(tmp1);
+					treasureDeck.shuffle();
+					tmp1 = treasureDeck.drawCard();
+				}
+				**/
+				
+				// Card tmp2 = treasureDeck.drawCard();
+				/**
+				// check if water rise card is drawn and repeat draw as necessary
+				while (tmp2.getType() == CardType.Water_Rise){
+					treasureDeck.addCard(tmp2);
+					treasureDeck.shuffle();
+					tmp2 = treasureDeck.drawCard();
+				}
+				**/
+				
+				// give to player
+				// player[p].giveCard(tmp1);
+				// player[p].giveCard(tmp2);
+				
+				// 2-card draw (w/o water rise checks) will be repeated during each player turn
+				// -> bundle into method!
+			}
 			
 			// Set the water level
 			System.out.println("Set the water level between 1 - 5. Enter water level:");
@@ -91,8 +166,18 @@ public class Main {
 			System.out.println("Finished game set up. Get ready to play.\n");
 			
 			
+			/**
+			 * Main round-by-round loop
+			 */
+			int round = 0;
+			 while (win != true) {
+				 
+				 // TODO play
+			 
+			 }
 	
-			// Check if user wants to start a new game
+			
+			// Check if user wants to start another game
 			newGame = false;			// reset flag
 			do {
 				System.out.println("Would you like to play another game? [y/n] ");
