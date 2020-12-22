5e49d4f3934a0793c993aa2fc6eccbb5399a9e7c
davidgormley <54341250+davidgormley@users.noreply.github.com>
2020-12-22 12:51:07 +0000

    Updates to Player Class and Adding Comments throughout project

--- Forbidden Island/src/cards/WaterMeter.java
+++ Forbidden Island/src/cards/WaterMeter.java
@@ -1,13 +1,37 @@
 package cards;
 
+/**
+ * Class for the island tiles on the board
+ * 
+ * @author:  Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
+ * @date:    201223
+ * @version: 1.0
+ */
+
 public class WaterMeter {
-private int level=0;
-
-public int getLevel() {
-	return level;
-}
-
-public void setLevel(int level) {
-	this.level = level;
-}
+	
+	//===========================================================
+    // Variable Setup
+    //===========================================================
+	private int level = 0;
+	
+	
+	//===========================================================
+	// Getters and Setters
+	//===========================================================
+	/**
+	 * gets the water meter level
+	 * @return level Water meter level
+	 */
+	public int getLevel() {
+		return level;
+	}
+	
+	/**
+	 * sets the water meter level
+	 * @param level Water meter level
+	 */
+	public void setLevel(int level) {
+		this.level = level;
+	}
 }
--- Forbidden Island/src/deck/Deck.java
+++ Forbidden Island/src/deck/Deck.java
@@ -1,28 +1,52 @@
 package deck;
 
-import java.util.Stack;
-
 import cards.Card;
 import cards.CardType;
 import cards.IslandTile;
 import java.util.Collections;
+import java.util.Stack;
+
+/**
+ * Class for a Deck on the Board in a game of Forbidden Island.
+ * 
+ * @author:  Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
+ * @date:    201223
+ * @version: 1.0
+ */
 
 public class Deck<T> {
+	
+	//===========================================================
+	// Variable Setup
+	//===========================================================
 	Stack<T> deck = new Stack<T>();
 	
-
+	//===========================================================
+	// Getters and Setters
+	//===========================================================
+	/**
+	 * Add a card to the deck
+	 * @param e Adding card to the deck
+	 */
 	public void addCard(Card e) {
 		this.deck.add((T) e);
 	}
 
-	
-
+	/**
+	 * Take a card from the deck
+	 * @return Take a card from the deck
+	 */
 	public T drawCard() {
-
 		return deck.pop();
 	}
 
-
+	//===========================================================
+	// Other
+	//===========================================================
+	/**
+	 * Shuffule the deck
+	 * 
+	 */
 	public void shuffleDeck() {
 		Collections.shuffle(this.deck);
 	}
