c8da8a5e777aea13be89b0384294cd7eb3fa8a46
Owen RH <61594228+arpeggiowen@users.noreply.github.com>
2020-12-23 20:32:21 +0000

    FloodDiscard singleton

---
+++ Forbidden Island/src/deck/FloodDiscard.java
@@ -0,0 +1,47 @@
+package deck;
+
+import cards.Card;
+
+/**
+ * Singleton class for the Flood deck discard pile.
+ * 
+ * @author:  Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
+ * @date:    201223
+ * @version: 1.0
+ */
+
+public class FloodDiscard extends Deck<Card>{
+	//===========================================================
+	// Variable Setup
+	//===========================================================
+	private Deck<Card>				floodDiscard;
+	private static FloodDiscard 	fDeck;
+	
+	//===========================================================
+	// Private Constructor
+	//===========================================================
+	private FloodDiscard() {
+		this.floodDiscard = new Deck<Card>();
+	}
+	
+	//===========================================================
+	// Get Instance
+	//===========================================================
+	public static FloodDiscard getInstance() {
+		if (fDeck == null) {
+			fDeck = new FloodDiscard();
+		}
+		return fDeck;
+	}
+	
+	//===========================================================
+	// Public Methods
+	//===========================================================
+	/**
+	 * Method to empty the discard pile.
+	 */
+	public void empty() {
+		this.floodDiscard = null;
+	}
+
+}
