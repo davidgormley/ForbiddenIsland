8e5f80626aea1d6592c4d7bb470be28c5e26e4f6
Owen RH <61594228+arpeggiowen@users.noreply.github.com>
2020-12-28 21:44:40 +0000

    Sandbag action placeholder

---
+++ Forbidden Island/src/play/Sandbag.java
@@ -0,0 +1,27 @@
+package play;
+
+/**
+ * This class handles the sandbag action for players.
+ * 
+ * @author:  Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
+ * @date:    201223
+ * @version: 1.0
+ */
+
+public class Sandbag {
+	// check which player wishes to play the card
+	
+	// check if player has the card
+	
+	// check whether the target tile can be shored up (i.e., is not DRY or SUNK)
+	
+	
+	//===========================================================
+    // Variable Setup
+    //===========================================================
+	
+	
+	//===========================================================
+    // Methods
+    //===========================================================
+}
