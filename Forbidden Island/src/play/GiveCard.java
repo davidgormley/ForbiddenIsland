28e66140379d6e362b9a41249d7f3a3f58806b0f
Owen RH <61594228+arpeggiowen@users.noreply.github.com>
2020-12-28 21:15:42 +0000

    placeholder for GiveCard action class

---
+++ Forbidden Island/src/play/GiveCard.java
@@ -0,0 +1,33 @@
+package play;
+
+import cards.Card;
+
+/**
+ * This class handles the give card action for players.
+ * 
+ * @author:  Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
+ * @date:    201223
+ * @version: 1.0
+ */
+
+public class GiveCard {
+	// Select card to give
+	
+	// Select receiving player
+	// - show available players
+	// - check that receiver has space in inventory
+	
+	// Remove from P1's inventory and add to P2's
+	
+	
+	//===========================================================
+    // Variable Setup
+    //===========================================================
+	
+	
+	//===========================================================
+    // Methods
+    //===========================================================
+	
+	
+}
