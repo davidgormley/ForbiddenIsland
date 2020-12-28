9a29e98fcebf061b90080c219f800d9768a36ca4
Owen RH <61594228+arpeggiowen@users.noreply.github.com>
2020-12-28 20:40:55 +0000

    Created gameplay package and template for Move class

---
+++ Forbidden Island/src/play/Move.java
@@ -0,0 +1,31 @@
+package play;
+
+/**
+ * This class handles the move action for players.
+ * 
+ * @author:  Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
+ * @date:    201223
+ * @version: 1.0
+ */
+
+public class Move {
+	// Determine valid moves
+	// - check adjacent tiles (flood state; on the gameboard, etc.)
+	// - present as directional moves (North, South, etc.) or as numbered list of tiles?
+	
+	// Move player to desired tile
+	
+	// View map option
+	
+	//===========================================================
+    // Variable Setup
+    //===========================================================
+	
+	
+	//===========================================================
+    // Methods
+    //===========================================================
+	
+	
+	
+}
