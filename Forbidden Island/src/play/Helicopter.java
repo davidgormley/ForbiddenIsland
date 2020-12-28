1931ae196f8033e24d77b5bebd1facd388ce3a65
Owen RH <61594228+arpeggiowen@users.noreply.github.com>
2020-12-28 21:47:09 +0000

    Helicopter action placeholder

---
+++ Forbidden Island/src/play/Helicopter.java
@@ -0,0 +1,31 @@
+package play;
+
+/**
+ * This class handles the helicopter action for players.
+ * 
+ * @author:  Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
+ * @date:    201223
+ * @version: 1.0
+ */
+
+public class Helicopter {
+	// check which player wishes to play the card
+	
+	// check if player has the card
+	
+	// check which players are to be moved
+	
+	// select destination
+	
+	// move the players
+	
+	
+	//===========================================================
+    // Variable Setup
+    //===========================================================
+	
+	
+	//===========================================================
+    // Methods
+    //===========================================================
+}
