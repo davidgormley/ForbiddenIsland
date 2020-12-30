ce151750b06e16dc98bdeadd4843ad41148a0c74
davidgormley <54341250+davidgormley@users.noreply.github.com>
2020-12-30 12:48:15 +0000

    Update enumeration for describing the various types of treasure

--- Forbidden Island/src/cards/TreasureType.java
+++ Forbidden Island/src/cards/TreasureType.java
@@ -4,10 +4,14 @@ package cards;
  * Enumeration for describing the various types of treasure.
  * 
  * @author: Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
- * @date: 201223
+ * @date: 201230
  * @version: 1.0
  */
 
 public enum TreasureType {
-	EARTH_STONE, CRYSTAL_OF_FIRE, STATUE_OF_WIND, OCEAN_CHALICE, NONE
+	EARTH_STONE,
+	CRYSTAL_OF_FIRE,
+	STATUE_OF_WIND,
+	OCEAN_CHALICE,
+	NONE
 }
