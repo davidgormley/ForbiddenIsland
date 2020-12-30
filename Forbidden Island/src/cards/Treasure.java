ef6dfad67a2663bb365c86695593d0f7c085ffb6
davidgormley <54341250+davidgormley@users.noreply.github.com>
2020-12-30 12:46:22 +0000

    Update to Treasure class and cleaned comments

--- Forbidden Island/src/cards/Treasure.java
+++ Forbidden Island/src/cards/Treasure.java
@@ -4,7 +4,7 @@ package cards;
  * Class for the island tiles on the board
  * 
  * @author:  Owen Ryan-Hanbury, David Gormley and Srinithi Ramprasad
- * @date:    201223
+ * @date:    201230
  * @version: 1.0
  */
 
@@ -35,10 +35,18 @@ public class Treasure {
 		this.name = name;
 	}
 	
+	/**
+	 * sets the Treasure name
+	 * @return captured Boolean for whether the treasure is captured
+	 */
 	public boolean isCaptured() {
 		return captured;
 	}
 	
+	/**
+	 * sets Treasure to be captured
+	 * @param captured Boolean for whether the treasure is captured
+	 */
 	public void setCaptured(boolean captured) {
 		this.captured = captured;
 	}
