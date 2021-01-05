62d7baf7ac58f92f0bcca82c506e1d95ff2e3da3
Srinithi97 <srinithi.ramprasad@ucdconnect.ie>
2021-01-05 14:48:18 +0000

    Update AdventurersTest.java

--- Forbidden Island/tests/AdventurersTest.java
+++ Forbidden Island/tests/AdventurersTest.java
@@ -11,11 +11,25 @@ public class AdventurersTest {
     @Test
     public void test_adventurers_treasure_captures() {
         Adventurers adventurers = Adventurers.getInstance();
-
         Assertions.assertEquals(adventurers.haveTreasure(TreasureType.EARTH_STONE), false);
 
         adventurers.addTreasure(TreasureType.EARTH_STONE);
-
         Assertions.assertEquals(adventurers.haveTreasure(TreasureType.EARTH_STONE), true);
+    }
+
+    @Test
+    public void test_if_players_added() {
+        Adventurers adventurers = Adventurers.getInstance();
+
+        Player p1 = new Player("p1");
+        Player p2 = new Player("p2");
+        Player p3 = new Player("p3");
+
+        adventurers.addPlayer(1, p1);
+        adventurers.addPlayer(2, p2);
+        adventurers.addPlayer(3, p3);
+
+        Assertions.assertEquals(adventurers.numPlayers(), 3);
+        Assertions.assertNotEquals(adventurers.numPlayers(), 4);
     }
 }
